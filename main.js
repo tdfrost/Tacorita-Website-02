47ee808218a12a02f18e2b21716256a007131b3b
Taylor <tas0018@auburn.edu>
2020-06-06 10:33:22 -0500

    Finished Menu page foundation

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ menuToggler.addEventListener('click',function() {
     body.classList.toggle('open');
 }
 );
+
+//get the button navs
+let tabButtons = document.querySelectorAll('.tab-links');
+
+//get all content
+let tabContents = document.querySelectorAll('.tab-content');
+
+window.onload = function(){
+    document.getElementById('default-open').style.display = "block";
+}
+
+// Hide content function
+const hideContent = function() {
+    tabContents.forEach(content => {
+        content.style.display = "none"
+    })
+}
+
+// adding event listen for the buttons
+tabButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        let contentClass = `${button.classList[1]}-content`
+        hideContent();
+        document.querySelector(`.${contentClass}`).style.display = "block";
+    })
+})
