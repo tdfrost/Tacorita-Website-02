6787acb1a20a0e4e42b54d5942e4c4002d775c18
Taylor <tas0018@auburn.edu>
2020-06-22 14:29:55 -0500

    Finished animiations for sections/scroll reveal

--- main.js
+++ main.js
@@ -36,3 +36,61 @@ tabButtons.forEach(button => {
         document.querySelector(`.${contentClass}`).style.display = "block";
     })
 })
+
+//Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-col-one', {
+    origin: 'top',
+    duration: 1500,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-col-two', {
+    origin: 'top',
+    duration: 1500,
+    distance: '25rem',
+    delay: 900
+});
+
+sr.reveal('.animate-col-three', {
+    origin: 'top',
+    duration: 1500,
+    distance: '25rem',
+    delay: 1200
+});
+sr.reveal('.animate-col-four', {
+    origin: 'top',
+    duration: 1500,
+    distance: '25rem',
+    delay: 1500
+});
